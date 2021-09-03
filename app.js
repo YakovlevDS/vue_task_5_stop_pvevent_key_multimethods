5dcf4f891b6ff4e5c5fa1df89706f1a1bf1226d7
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-03 08:45:11 +0200

    added keyup shift enter exact click.right.prevent some methods by 1 event

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const App = {
-    data: () => ({
-        myHtml:'<h1>Vue3</h1>',
-        title: "VUE_3",
-        arr: ["Hi", "Hello", "Good_Day", "Good_Everning"],
-        obj: {
-            age: 34,
-            weight: 60,
-            mass:80
-        },
-        items:[1,2,3,4,5,6,7],
-    }),
-    computed: {
-        evenItems() {
-            return this.items.filter(i=>i % 2===0)
-       }
-        },
-    
-}
+  data: () => ({
+    myHtml: "<h1>Vue3</h1>",
+    title: "VUE_3",
+    arr: ["Hi", "Hello", "Good_Day", "Good_Everning"],
+    obj: {
+      age: 34,
+      weight: 60,
+      mass: 80,
+    },
+    items: [1, 2, 3, 4, 5, 6, 7],
+  }),
+  methods: {
+    addItem(e) {
+      this.items.unshift(this.$refs.myInput.value);
+      this.$refs.myInput.value = "";
+     
+    },
+    remove(i) {
+      this.items.splice(i, 1);
+    },
+    log(item) {
+      console.log(item);
+    },
+  },
+
+  computed: {
+    evenItems() {
+      return this.items.filter((i) => i % 2 === 0);
+    },
+  },
+};
 Vue.createApp(App).mount(`#app`)
